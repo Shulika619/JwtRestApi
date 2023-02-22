59148ececbb1f4b943ae6c25aa2be2c48d5a6306
Sergey Shulika <developer.shulika@gmail.com>
2023-02-22 03:41:31 +0300

    initial commit

---
+++ src/main/java/com/example/jwtrestapi/model/BaseEntity.java
@@ -0,0 +1,4 @@
+package com.example.jwtrestapi.model;
+
+public class BaseEntity {
+}
---
+++ src/test/java/com/example/jwtrestapi/JwtRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
